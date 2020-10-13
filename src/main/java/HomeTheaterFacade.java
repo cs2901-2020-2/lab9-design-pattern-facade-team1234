dc9667d3107f810a91e8da1114e9b4e85a11dd45
Mauricio <mauricio.pinto@utec.edu.pe>
2020-10-13 10:45:02 -0500

    facade class

--- src/main/java/HomeTheaterFacade.java
+++ src/main/java/HomeTheaterFacade.java
@@ -1,3 +1,19 @@
 public class HomeTheaterFacade {
+    Amplifier amplifier;
+    DVDPlayer dvdPlayer;
+    CDPlayer cdPlayer;
+    Projector projector;
+    Screen screen;
+    TheaterLights theaterLights;
+    PopCornPopper popCornPopper;
 
+    public HomeTheaterFacade () {}
+
+    public void PlayMovie () {
+        Amplifier.On ();
+    }
+
+    public void EndMovie () {
+
+    }
 }
